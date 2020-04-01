f4d1f069722b65250a4a15aa816745740f801e38
navathareddy <navatha.reddy1@gmail.com>
2020-04-01 13:10:57 -0400

    Added movie feature

---
+++ GitTest/GitTest/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GitTest.Models;
+
+
+namespace GitTest.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "navatha" };
+
+            return View(movie);
+        }
+    }
+}
